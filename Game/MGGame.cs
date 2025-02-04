e41eb5b60db5782cfb9b62977f21b523c3eb3238
CasperHuikeshoven <casperhuik05@gmail.com>
2025-02-04 15:11:08 +0100

    Setup project

---
+++ Game/MGGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+class MGGame : Game
+{
+    private GraphicsDeviceManager graphics;
+    private SpriteBatch spriteBatch;
+
+    [STAThread]
+    static void Main()
+    {
+        MGGame game = new MGGame();
+        game.Run();
+    }
+
+    public MGGame()
+    {
+        graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        spriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        
+    }
+
+    protected void HandleInput()
+    {
+
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+
+        spriteBatch.Begin();
+        spriteBatch.End();
+    }
+
+}
